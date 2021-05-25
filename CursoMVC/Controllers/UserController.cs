852e6cb628f4ea1194b27737dfffb8719eb89c15
Eliana Velez <eliana.marcela.velez@gmail.com>
2021-05-25 15:59:30 -0500

    Se crea bd para registrar y manejar usuarios

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CursoMVC.Models.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,49 @@ namespace CursoMVC.Controllers
 {
     public class UserController : Controller
     {
+        private UserDBContext db = new UserDBContext();
+
         // GET: User
         public ActionResult RegisterUser()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Método encargado de recibir desde la vista un objeto para el registro de usuarios
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult RegisterUser(UserModel model)
+        {
+            try
+            {
+                model.Perfil = "Nivel_1";
+
+                // Se agrega el modelo al dbContext
+                db.User.Add(model);
+
+                // Guarda los cambio
+                db.SaveChanges();
+
+                return RedirectToAction("ListUser");
+
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult ListUser()
+        {
+            List<UserModel> listUser = (from usuario in db.User
+                                        orderby usuario.Cedula
+                                        select usuario).ToList();
+
+            return View(listUser);
         }
     }
 }
--- CursoMVC/Models/Users/UserModel.cs
+++ CursoMVC/Models/Users/UserModel.cs
@@ -1,18 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
 
 namespace CursoMVC.Models.Users
 {
     public class UserModel
     {
         public string Nombre { get; set; }
+
         public string Apellido { get; set; }
+
+        [Key]
         public string Cedula { get; set; }
+
         public string Email { get; set; }
+
         public string Perfil { get; set; }
+
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext() { }
+
+        public DbSet<UserModel> User { get; set; }
 
     }
 }
