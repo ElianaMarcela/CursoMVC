de198a325af7540b6a3bfca627f8c9a89e08074e
Eliana Velez <eliana.marcela.velez@gmail.com>
2021-05-26 15:52:08 -0500

    Se complementa el CRUD de usuarios

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -36,7 +36,6 @@ namespace CursoMVC.Controllers
                 db.SaveChanges();
 
                 return RedirectToAction("ListUser");
-
             }
             catch (Exception ex)
             {
@@ -44,7 +43,10 @@ namespace CursoMVC.Controllers
             }
         }
 
-
+        /// <summary>
+        /// Método encargado de Listar los usuarios que se encuentran creados
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ListUser()
         {
             List<UserModel> listUser = (from usuario in db.User
@@ -52,6 +54,77 @@ namespace CursoMVC.Controllers
                                         select usuario).ToList();
 
             return View(listUser);
+        }
+
+        /// <summary>
+        /// Método encargado de consultar el usuario seleccionado 
+        /// a través del ID (Cedula)
+        /// </summary>
+        /// <param name="id">Dato clave único del usuario</param>
+        /// <returns>Vista de tipo UserModel con el usuario consultado</returns>
+        public ActionResult EditUser(string id)
+        {
+            UserModel User = db.User.Single(edit => edit.Cedula == id);
+            return View(User);
+        }
+
+
+        /// <summary>
+        /// Método encargado de consultar el usuario seleccionado que será modificado 
+        /// y reflejado en base de datos
+        /// </summary>
+        /// <param name="cedula">Clave única del registro</param>
+        /// <param name="collection">Formulario Actual</param>
+        /// <returns>Vista actual o que lista los usuarios</returns>
+        [HttpPost]
+        public ActionResult EditUser(string cedula, FormCollection collection)
+        {
+            UserModel User = db.User.Single(edit => edit.Cedula == cedula);
+            if (TryUpdateModel(User))
+            {
+                db.SaveChanges();
+                return RedirectToAction("ListUser");
+            }
+
+            return View(User);
+        }
+
+        /// <summary>
+        /// Método encargado de consultar el usuario seleccionado que será eliminado 
+        /// y reflejado en base de datos
+        /// </summary>
+        /// <param name="id">Dato clave único del usuario</param>
+        /// <returns>Vista de tipo UserModel con el usuario consultado</returns>
+        public ActionResult DeleteUser(string id)
+        {
+            UserModel User = db.User.Single(delete => delete.Cedula == id);
+            return View(User);
+        }
+
+
+        /// <summary>
+        /// Método encargado de consultar el usuario seleccionado que será eliminado
+        /// y reflejado en base de datos
+        /// </summary>        
+        /// <param name="cedula">Clave única del registro</param>
+        /// <param name="collection">Formulario Actual</param>
+        /// <returns>Vista actual o que lista los usuarios</returns>
+        [HttpPost]
+        public ActionResult DeleteUser(string id, FormCollection collection)
+        {
+            UserModel User = db.User.Single(delete => delete.Cedula == id);
+            try
+            {
+                db.User.Remove(User);
+                db.SaveChanges();
+                return RedirectToAction("ListUser");
+            }
+            catch (Exception)
+            {
+                return View(User);
+            }
+
+
         }
     }
 }
